5cfa804cc76090266e5931155af4fc3b458f8bcf
ancoca <ancoca1993@gmail.com>
2016-03-29 18:03:17 +0200

    colocacion de las funciones en su lugar correcto (DAO/BLL)

--- src/framework/module/config/model/BLL/BLL_config.java
+++ src/framework/module/config/model/BLL/BLL_config.java
@@ -8,7 +8,6 @@ package framework.module.config.model.BLL;
 import framework.module.config.model.DAO.DAO_config;
 import framework.module.config.model.classes.ClassConfig;
 import framework.module.config.model.functions.json_auto_config;
-import framework.module.config.view.Config;
 
 /**
  *
@@ -17,107 +16,19 @@ import framework.module.config.view.Config;
 public class BLL_config {
     
     public static void setConfig () {
-        ClassConfig.getInstance().setLanguage(DAO_config.language());
-        ClassConfig.getInstance().setDate(DAO_config.date());
-        ClassConfig.getInstance().setCurrency(DAO_config.currency());
-        ClassConfig.getInstance().setTheme(DAO_config.theme());
-        ClassConfig.getInstance().setDecimal(DAO_config.decimal());
+        ClassConfig.getInstance().setLanguage(DAO_config.setlanguage());
+        ClassConfig.getInstance().setDate(DAO_config.setdate());
+        ClassConfig.getInstance().setCurrency(DAO_config.setcurrency());
+        ClassConfig.getInstance().setTheme(DAO_config.settheme());
+        ClassConfig.getInstance().setDecimal(DAO_config.setdecimal());
         json_auto_config.savejson_config();
     }
     
     public static void getConfig () {
-        BLL_config.getlanguage();
-        BLL_config.getdate();
-        BLL_config.getcurrency();
-        BLL_config.gettheme();
-        BLL_config.getdecimal();
-    }
-    
-    public static void getlanguage () {
-        switch (ClassConfig.getInstance().getLanguage()){
-            case "en":
-                Config.english.setSelected(true);
-                break;
-            case "es":
-                Config.castellano.setSelected(true);
-                break;
-            case "val":
-                Config.valencia.setSelected(true);
-                break;
-        }
-    }
-    
-    public static void getdate () {
-        switch (ClassConfig.getInstance().getDate()){
-            case "dd/MM/yyyy":
-                Config.date1.setSelected(true);
-                break;
-            case "dd-MM-yyyy":
-                Config.date2.setSelected(true);
-                break;
-            case "yyyy/MM/dd":
-                Config.date3.setSelected(true);
-                break;
-            case "yyyy-MM--dd":
-                Config.date4.setSelected(true);
-                break;
-        }
-    }
-    
-    public static void getcurrency () {
-        switch (ClassConfig.getInstance().getCurrency()){
-            case '€':
-                Config.€.setSelected(true);
-                break;
-            case '$':
-                Config.$.setSelected(true);
-                break;
-            case '£':
-                Config.£.setSelected(true);
-                break;
-        }
-    }
-    
-    public static void gettheme () {
-        switch (ClassConfig.getInstance().getTheme()){
-            case "METAL":// Metal - Predeterminado JAVA
-                Config.metal.setSelected(true);
-                break;
-            case "GTK":// GTK
-                Config.gtk.setSelected(true);
-                break;
-            case "MOTIF":// Motif
-                Config.motif.setSelected(true);
-                break;
-            case "NINBUS":// Nimbus - JAVA
-                Config.ninbus.setSelected(true);
-                break;	
-            case "WINDOWS95":// WINDOWS 95
-                Config.windowsclassic.setSelected(true);
-                break;
-            case "WINDOWS":// WINDOWS
-                Config.windows.setSelected(true);
-                break;
-            case "MAC OS":// MAC OS
-                Config.mac.setSelected(true);
-                break;
-            case "MAC OS X":// MAC OS X
-                Config.aqua.setSelected(true);
-                break;
-        }
-    }
-    
-    public static void getdecimal () {
-        switch (ClassConfig.getInstance().getDecimal()){
-            case 1:
-                Config.decimal1.setSelected(true);
-                break;
-            case 2:
-                Config.decimal2.setSelected(true);
-                break;
-            case 3:
-                Config.decimal3.setSelected(true);
-                break;
-        }
+        DAO_config.getlanguage();
+        DAO_config.getdate();
+        DAO_config.getcurrency();
+        DAO_config.gettheme();
+        DAO_config.getdecimal();
     }
 }
